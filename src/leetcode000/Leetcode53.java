66d2ebba6f3aaf55f3ba64ed34efd3ae3d1fecf9
AXIMI <2018837008@qq.com>
2019-10-31 21:50:24 +0800

    daily archieve

--- src/leetcode000/Leetcode53.java
+++ src/leetcode000/Leetcode53.java
@@ -9,16 +9,16 @@ public class Leetcode53 {
 	}
 
 }
-
-class Solution {
-    public int maxSubArray(int[] nums) {
-    	int curSum=nums[0],res=nums[0];
-    	
-    	for(int i=1,len=nums.length;i<len;i++) {
-    		if(curSum+nums[i]<nums[i])	curSum=nums[i];
-    		else	curSum+=nums[i];
-    		res=curSum>res?curSum:res;
-    	}
-    	return res;
-    }
-}
+//
+//class Solution {
+//    public int maxSubArray(int[] nums) {
+//    	int curSum=nums[0],res=nums[0];
+//    	
+//    	for(int i=1,len=nums.length;i<len;i++) {
+//    		if(curSum+nums[i]<nums[i])	curSum=nums[i];
+//    		else	curSum+=nums[i];
+//    		res=curSum>res?curSum:res;
+//    	}
+//    	return res;
+//    }
+//}
